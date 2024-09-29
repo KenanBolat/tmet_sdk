671d4b1c8dea0c68e0689697d606a50c29717c64
KenanBolat <kenan23@gmail.com>
2024-09-29 10:53:37 +0300

    fix channel issue

--- dataconverter/communication/message_broker_if.py
+++ dataconverter/communication/message_broker_if.py
@@ -8,29 +8,41 @@ class RabbitMQInterface:
         self.username = username
         self.password = password
         self.queue_name = queue_name
+        self.connection = None
+        self.channel = None
 
     def connect(self):
-        credentials = pika.PlainCredentials(self.username, self.password)
-        parameters = pika.ConnectionParameters(self.host, self.port, '/', credentials,
-                                               heartbeat=600, blocked_connection_timeout=300)
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        channel.queue_declare(queue=self.queue_name)
-        return channel
+        if not self.connection or self.connection.is_closed:
+            credentials = pika.PlainCredentials(self.username, self.password)
+            parameters = pika.ConnectionParameters(self.host, self.port, '/', credentials,
+                                                   heartbeat=600, blocked_connection_timeout=300)
+            connection = pika.BlockingConnection(parameters)
+            self.channel = connection.channel()
+            self.channel.queue_declare(queue=self.queue_name)
+        return self.channel
 
     def send(self, message):
-        channel = self.connect()
-        channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
-        channel.close()
+        if not self.connection or self.connection.is_closed:
+            self.connect()
+
+        self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
+        self.channel.close()
 
     def receive(self):
-        channel = self.connect()
-        method_frame, header_frame, body = channel.basic_get(queue=self.queue_name, auto_ack=True)
+        if not self.connection or self.connection.is_closed:
+            self.channel = self.connect()
+        method_frame, header_frame, body = self.channel.basic_get(queue=self.queue_name, auto_ack=True)
         if method_frame:
-            channel.basic_ack(method_frame.delivery_tag)
+            ## Process the message
+            self.channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+            ## Acknowledge the message
             return body.decode('utf-8')
         else:
             return None
+
+    def close(self):
+        if self.connection and self.connection.is_open:
+            self.connection.close()
 
     @staticmethod
     def get_ip():
